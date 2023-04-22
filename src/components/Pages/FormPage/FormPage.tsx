97d860872987bf4d1d2536221c418956d7b26295
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2023-04-22 10:19:52 +0200

    fix: add header

--- src/components/Pages/FormPage/FormPage.tsx
+++ src/components/Pages/FormPage/FormPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Form from "../../Form/Form";
 import CardsForm from "../../Form/CardsForm/CardsForm";
+import Header from "../Header/Header";
 
 const FormPage: React.FC = () => {
   return (
     <>
+      <Header />
       <Form />
       <CardsForm />
     </>
